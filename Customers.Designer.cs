a955427d59fad29375042dd74ebdff6bc14a2197
aaaavq <quanduong06099@gmail.com>
2025-12-10 17:47:00 +0700

    Create Customers.Designer.cs

---
+++ Customers.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace coffee
+{
+    partial class Customers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.lbCustomertype = new System.Windows.Forms.Label();
+            this.lbThismonth = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.lbCustomercode = new System.Windows.Forms.Label();
+            this.txtCustomerCode = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.lbLastmonth = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dtgCustomer = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgCustomer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnBack);
+            this.panel1.Controls.Add(this.lbCustomertype);
+            this.panel1.Controls.Add(this.lbThismonth);
+            this.panel1.Controls.Add(this.txtSearch);
+            this.panel1.Controls.Add(this.lbCustomercode);
+            this.panel1.Controls.Add(this.txtCustomerCode);
+            this.panel1.Controls.Add(this.btnAdd);
+            this.panel1.Controls.Add(this.txtAddress);
+            this.panel1.Controls.Add(this.btnSearch);
+            this.panel1.Controls.Add(this.txtCustomerName);
+            this.panel1.Controls.Add(this.btnUpdate);
+            this.panel1.Controls.Add(this.btnDelete);
+            this.panel1.Controls.Add(this.txtPhone);
+            this.panel1.Controls.Add(this.lbLastmonth);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(869, 203);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(675, 86);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(124, 26);
+            this.btnBack.TabIndex = 33;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // lbCustomertype
+            // 
+            this.lbCustomertype.AutoSize = true;
+            this.lbCustomertype.Location = new System.Drawing.Point(51, 145);
+            this.lbCustomertype.Name = "lbCustomertype";
+            this.lbCustomertype.Size = new System.Drawing.Size(58, 16);
+            this.lbCustomertype.TabIndex = 20;
+            this.lbCustomertype.Text = "Address";
+            this.lbCustomertype.Click += new System.EventHandler(this.lbCustomertype_Click);
+            // 
+            // lbThismonth
+            // 
+            this.lbThismonth.AutoSize = true;
+            this.lbThismonth.Location = new System.Drawing.Point(51, 112);
+            this.lbThismonth.Name = "lbThismonth";
+            this.lbThismonth.Size = new System.Drawing.Size(46, 16);
+            this.lbThismonth.TabIndex = 19;
+            this.lbThismonth.Text = "Phone";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(465, 154);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(159, 22);
+            this.txtSearch.TabIndex = 32;
+            // 
+            // lbCustomercode
+            // 
+            this.lbCustomercode.AutoSize = true;
+            this.lbCustomercode.Location = new System.Drawing.Point(51, 41);
+            this.lbCustomercode.Name = "lbCustomercode";
+            this.lbCustomercode.Size = new System.Drawing.Size(100, 16);
+            this.lbCustomercode.TabIndex = 17;
+            this.lbCustomercode.Text = "Customer Code";
+            this.lbCustomercode.Click += new System.EventHandler(this.lbCustomername_Click);
+            // 
+            // txtCustomerCode
+            // 
+            this.txtCustomerCode.Location = new System.Drawing.Point(205, 38);
+            this.txtCustomerCode.Name = "txtCustomerCode";
+            this.txtCustomerCode.Size = new System.Drawing.Size(193, 22);
+            this.txtCustomerCode.TabIndex = 26;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(465, 34);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(124, 23);
+            this.btnAdd.TabIndex = 22;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(207, 142);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(192, 22);
+            this.txtAddress.TabIndex = 30;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(675, 153);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(124, 23);
+            this.btnSearch.TabIndex = 31;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtCustomerName
+            // 
+            this.txtCustomerName.Location = new System.Drawing.Point(205, 71);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(194, 22);
+            this.txtCustomerName.TabIndex = 25;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(675, 34);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(124, 23);
+            this.btnUpdate.TabIndex = 28;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(465, 89);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(124, 23);
+            this.btnDelete.TabIndex = 29;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(206, 106);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(193, 22);
+            this.txtPhone.TabIndex = 24;
+            // 
+            // lbLastmonth
+            // 
+            this.lbLastmonth.AutoSize = true;
+            this.lbLastmonth.Location = new System.Drawing.Point(51, 74);
+            this.lbLastmonth.Name = "lbLastmonth";
+            this.lbLastmonth.Size = new System.Drawing.Size(104, 16);
+            this.lbLastmonth.TabIndex = 18;
+            this.lbLastmonth.Text = "Customer Name";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::coffee.Properties.Resources.nyvKi8k;
+            this.pictureBox1.Location = new System.Drawing.Point(4, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(862, 191);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dtgCustomer);
+            this.panel2.Location = new System.Drawing.Point(12, 213);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(869, 307);
+            this.panel2.TabIndex = 1;
+            // 
+            // dtgCustomer
+            // 
+            this.dtgCustomer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgCustomer.Location = new System.Drawing.Point(13, 4);
+            this.dtgCustomer.Name = "dtgCustomer";
+            this.dtgCustomer.RowHeadersWidth = 51;
+            this.dtgCustomer.RowTemplate.Height = 24;
+            this.dtgCustomer.Size = new System.Drawing.Size(853, 294);
+            this.dtgCustomer.TabIndex = 0;
+            this.dtgCustomer.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgProduct_CellClick);
+            this.dtgCustomer.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgCustomer_CellContentClick);
+            this.dtgCustomer.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgCustomer_CellDoubleClick);
+            // 
+            // Customers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(946, 526);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Customers";
+            this.Text = "Order";
+            this.Load += new System.EventHandler(this.Customers_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgCustomer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.TextBox txtCustomerName;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label lbCustomertype;
+        private System.Windows.Forms.Label lbCustomercode;
+        private System.Windows.Forms.TextBox txtCustomerCode;
+        private System.Windows.Forms.Label lbLastmonth;
+        private System.Windows.Forms.Label lbThismonth;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dtgCustomer;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}

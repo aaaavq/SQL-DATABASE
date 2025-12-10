8edf0adea3d5f74c5105d6550d5147ffd3266b7f
aaaavq <quanduong06099@gmail.com>
2025-12-10 17:52:48 +0700

    Create Order_History .Designer.cs

---
+++ Order_History .Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace coffee
+{
+    partial class Order_History
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dtgOrderHistory = new System.Windows.Forms.DataGridView();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgOrderHistory)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dtgOrderHistory);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(674, 409);
+            this.panel1.TabIndex = 1;
+            // 
+            // dtgOrderHistory
+            // 
+            this.dtgOrderHistory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgOrderHistory.Location = new System.Drawing.Point(0, 0);
+            this.dtgOrderHistory.Name = "dtgOrderHistory";
+            this.dtgOrderHistory.RowHeadersWidth = 51;
+            this.dtgOrderHistory.RowTemplate.Height = 24;
+            this.dtgOrderHistory.Size = new System.Drawing.Size(668, 406);
+            this.dtgOrderHistory.TabIndex = 0;
+            this.dtgOrderHistory.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgProduct_CellContentClick);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(575, 438);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(108, 59);
+            this.btnBack.TabIndex = 2;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // Order_History
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 509);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.panel1);
+            this.Name = "Order_History";
+            this.Text = "Order_History";
+            this.Load += new System.EventHandler(this.OrderHistory_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgOrderHistory)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dtgOrderHistory;
+        private System.Windows.Forms.Button btnBack;
+    }
+}

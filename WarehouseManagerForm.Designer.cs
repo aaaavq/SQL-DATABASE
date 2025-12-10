7ad561a09c7ee487fe9c683a781e256d0819c7d7
aaaavq <quanduong06099@gmail.com>
2025-12-10 17:55:10 +0700

    Add WarehouseManagerForm designer code

---
+++ WarehouseManagerForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace coffee
+{
+    partial class WarehouseManagerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnManageProduct = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnManageProduct
+            // 
+            this.btnManageProduct.Location = new System.Drawing.Point(41, 24);
+            this.btnManageProduct.Name = "btnManageProduct";
+            this.btnManageProduct.Size = new System.Drawing.Size(226, 114);
+            this.btnManageProduct.TabIndex = 0;
+            this.btnManageProduct.Text = "Manage Product";
+            this.btnManageProduct.UseVisualStyleBackColor = true;
+            this.btnManageProduct.Click += new System.EventHandler(this.btnManageProduct_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::coffee.Properties.Resources.unnamed;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(530, 284);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // WarehouseManagerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 326);
+            this.Controls.Add(this.btnManageProduct);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "WarehouseManagerForm";
+            this.Text = "WarehouseManager";
+            this.Load += new System.EventHandler(this.WarehouseManagerForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnManageProduct;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
